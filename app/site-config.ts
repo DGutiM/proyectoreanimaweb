1a022ace60af5b293414a64015f3b322303528fa
DGutiM <diegogutimartinez@gmail.com>
2026-08-30 12:07:44 +0200

    Update site-config.ts

--- app/site-config.ts
+++ app/site-config.ts
@@ -1,3 +1,3 @@
-export const siteUrl = 'https://proyecto-reanima-vida-empresas.dgutim.chatgpt.site';
+export const siteUrl = 'https://www.proyectoreanimavida.com';
 export const socialImage = `${siteUrl}/og-reanima-v2.png`;
 export const contactEmail = 'proyectoreanimavida@gmail.com';
